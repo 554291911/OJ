5c1e24eed69f4bd2f95463cf3c2b2f344efa1e2a
zhouhao <554291911@qq.com>
2016-08-10 16:34:29 +0800

    用一个栈实现对另一个栈的排序

---
+++ src/one/twoStackSort/TwoStackSort.java
@@ -0,0 +1,18 @@
+package one.twoStackSort;
+
+import java.util.Stack;
+
+public class TwoStackSort {
+
+	public void sortStackByStack(Stack<Integer> stack) {
+		Stack<Integer> help = new Stack<Integer>();
+		while (!stack.isEmpty()) {
+			int value = stack.pop();
+			while (!help.isEmpty() && help.peek() > value)
+				stack.push(help.pop());
+			stack.push(value);
+		}
+		while (!help.isEmpty())
+			stack.push(help.pop());
+	}
+}
