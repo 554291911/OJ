196bb06304bd5bd2a3fbe9a927a876061ddb376d
zhouhao <554291911@qq.com>
2016-08-10 20:15:01 +0800

    双端队列生成窗口最大值数组

---
+++ src/two/windowMaxArr/WindowMaxArr.java
@@ -0,0 +1,24 @@
+package two.windowMaxArr;
+
+import java.util.LinkedList;
+
+public class WindowMaxArr {
+	
+	public int[] getMax(int[] arr, int w) {
+		if (arr == null || w < 1 || arr.length < w)
+			return null;
+		LinkedList<Integer> deque = new LinkedList<Integer>();
+		int[] res = new int[arr.length - w + 1];
+		int index = 0;
+		for (int i = 0; i < arr.length; i++) {
+			while (!deque.isEmpty() && arr[deque.peekLast()] <= arr[i])
+				deque.pollLast();
+			deque.addLast(i);
+			if (deque.peekFirst() == i - w)// 判断下标是否过期，如果过期，抛弃
+				deque.pollFirst();
+			if (i >= w - 1)// 前2个特殊，后面的都要放
+				res[index++] = arr[deque.peekFirst()];
+		}
+		return res;
+	}
+}
