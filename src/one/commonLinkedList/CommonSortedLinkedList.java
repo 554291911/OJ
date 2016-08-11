b353318750a44a2f0a6132202b25a02a072b2f91
zhouhao <554291911@qq.com>
2016-08-11 20:11:02 +0800

    打印两个有序链表的公共部分

---
+++ src/one/commonLinkedList/CommonSortedLinkedList.java
@@ -0,0 +1,29 @@
+package one.commonLinkedList;
+
+public class CommonSortedLinkedList {
+
+	public void printCommonList(Node head1, Node head2) {
+		while (head1 != null && head2 != null) {
+			if (head1.value < head2.value)
+				head1 = head1.next;
+			else
+				if (head1.value > head2.value)
+					head2 = head2.next;
+				else {
+					System.out.print(head1.value + " ");
+					head1 = head1.next;
+					head2 = head2.next;
+				}
+		}
+	}
+}
+
+class Node {
+	
+	public int value;
+	public Node next;
+	
+	public Node(int data) {
+		this.value = data;
+	}
+}
