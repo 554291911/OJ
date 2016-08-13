c69f1da6299d1021b4576a02b85d8b7b3ef4de98
zhouhao <554291911@qq.com>
2016-08-13 16:50:23 +0800

    判断一个链表是否为回文结构

---
+++ src/one/palindromeList/PalindromeList.java
@@ -0,0 +1,52 @@
+package one.palindromeList;
+
+import java.util.Stack;
+
+public class PalindromeList {
+	
+	public boolean palindrome1(Node head) {
+		if (head == null || head.next == null)
+			return true;
+		Stack<Node> stack = new Stack<Node>();
+		Node cur = head;
+		while (cur != null) {
+			stack.push(cur);
+			cur = cur.next;
+		}
+		while (head != null && !stack.isEmpty()) {
+			if (head.value != stack.pop().value)
+				return false;
+			head = head.next;
+		}
+		return true;
+	}
+	
+	public boolean palindrome2(Node head) {
+		if (head == null || head.next == null)
+			return true;
+		Stack<Node> stack = new Stack<Node>();
+		Node left = head;
+		Node right = head.next;
+		while (left.next != null && left.next.next != null) {
+			stack.push(right);
+			right = right.next;
+		}
+		while (!stack.isEmpty()) {
+			if (head.value != stack.pop().value)
+				return false;
+			head = head.next;
+		}
+		return true;
+	}
+
+}
+
+class Node {
+	
+	public int value;
+	public Node next;
+	
+	public Node(int data) {
+		this.value = data;
+	}
+}
