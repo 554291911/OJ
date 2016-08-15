0d81f8b92ee2afebcc3fe0991ac7e08239cb9409
zhouhao <554291911@qq.com>
2016-08-15 20:24:35 +0800

    双向链表

---
+++ src/commonDataStructure/doubleNode/Node.java
@@ -0,0 +1,12 @@
+package commonDataStructure.doubleNode;
+
+public class Node {
+	
+	public int value;
+	public Node left;
+	public Node right;
+
+	public Node(int data) {
+		this.value = data;
+	}
+}
