9fc8f0e64eef4b025d8a798b1f01825acce9e71d
zhouhao <554291911@qq.com>
2016-08-15 14:33:14 +0800

    随机指针节点

---
+++ src/commonDataStructure/randomNode/Node.java
@@ -0,0 +1,12 @@
+package commonDataStructure.randomNode;
+
+public class Node {
+	
+	public int value;
+	public Node next;
+	public Node rand;
+
+	public Node(int data) {
+		this.value = data;
+	}
+}
