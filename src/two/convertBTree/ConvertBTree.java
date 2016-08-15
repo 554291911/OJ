d6d16a65ee68e818b850a206044664fbfad6478a
zhouhao <554291911@qq.com>
2016-08-15 20:25:00 +0800

    将二叉搜索树转换成双向链表

---
+++ src/two/convertBTree/ConvertBTree.java
@@ -0,0 +1,34 @@
+package two.convertBTree;
+
+import java.util.LinkedList;
+
+import commonDataStructure.doubleNode.Node;
+
+public class ConvertBTree {
+
+	public Node convert(Node head) {
+		LinkedList<Node> list = new LinkedList<Node>();
+		inOrderToqueue(head, list);
+		head = list.poll();
+		Node pre = head;
+		pre.left = null;
+		Node cur = null;
+		while (!list.isEmpty()) {
+			cur = list.poll();
+			pre.right = cur;
+			cur.left = pre;
+			pre = cur;
+		}
+		pre.right = null;
+		return head;
+	}
+
+	private void inOrderToqueue(Node head, LinkedList<Node> list) {
+		if (head == null)
+			return;
+		inOrderToqueue(head.left, list);
+		list.offer(head);
+		inOrderToqueue(head.right, list);
+	}
+
+}
